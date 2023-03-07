b94768956c897c0f2200781d0ff26bdfea510287
rahul <rahulgheware123@gmail.com>
2023-03-07 22:33:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import{useEffect,useState} from 'react'
+import axios from 'axios'
+import {MathJaxContext,MathJax} from 'better-react-mathjax'
+import { question } from './componants/content';
+
+
+
 
 function App() {
+  const [data, setData] = useState("")
+  const [count, setCount] = useState(0);
+  
+  useEffect(() => {
+    
+    axios
+    .get(`https://0h8nti4f08.execute-api.ap-northeast-1.amazonaws.com/getQuestionDetails/getquestiondetails?QuestionID=${question[count]}`)
+    .then((response)=>
+     {setData(response.data[0].Question)
+      console.log(response.data)
+     })
+     
+  });
+  
+  const handlePrevClick=() =>{
+    console.log("Previos")
+    setCount(count-1);
+  }
+  const handleNextClick=() =>{
+    setCount(count+1);
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MathJaxContext>
+      <h2>Nioclass</h2>
+      <div className="items">
+        <MathJax className="content">{data}</MathJax>
+     </div>
+      <div className="container">
+        <button className='leftbutton' disabled={count<=0} onClick={handlePrevClick}>Previous</button>
+        <button className='rightbutton' disabled={count>=2} onClick={handleNextClick}>Next</button>
+      </div>
+    </MathJaxContext>
+        
   );
 }
 
---
+++ src/componants/content.js
@@ -0,0 +1,1 @@
+export const question=["AreaUnderTheCurve_901","BinomialTheorem_901","DifferentialCalculus2_901"]
